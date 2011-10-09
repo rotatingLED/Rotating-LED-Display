3c0b0f4f0b7f649bfd28b94f1f43000c532ece2d
David Halter <davidhalter88@gmail.com>
2011-10-09 23:55:38 +0200

    initial commit for PA project.

---
+++ src/types.h
@@ -0,0 +1,17 @@
+/*
+ * Rotating LED Display
+ * ZHAW PA Project
+ *
+ * Author: Andreas Butti (buttiand), David Halter (haltedav)
+ *
+ * Brief:
+ * Typedefinitions
+ */
+
+#ifndef __TYPES_H__
+#define __TYPES_H__
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#endif /* __TYPES_H__ */
