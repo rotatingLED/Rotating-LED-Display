4dbfbaf0293bb3bb7e9d2263ef6b31e3ee235b67
Andreas Butti <andreas@andreas.(none)>
2011-11-29 21:02:45 +0100

    Syncboard zwischenstatus

--- firmware/Syncboard/driver/hw.c
+++ firmware/Syncboard/driver/hw.c
@@ -3,19 +3,22 @@
  */
 #include <avr/io.h>
 #include <avr/interrupt.h>  // for sei()
+#include <util/atomic.h>
 
+#include "hw.h"
 #include "adc.h"
+
+uint32_t time = 0;
 
 void hw_init() {
 	// LEDs
-	DDRD = (1 << PD6) | (1 << PD7) ;
+	DDRD = (1 << PD6) | (1 << PD7);
 
 	// Initialize the ADC
 	adc_init();
 
-
 	// initialize blink
-	TCCR0B |= (1 << CS00) | (1 << CS02); // Prescaler: 1024
+	TCCR0B |= (1 << CS00) | (1 << CS02); // Prescaler: 1024: 12'000'000 / 1024 = 0.000085333s / Takt
 	TIMSK0 |= (1 << OCIE0A); // enable interrupt
 
 	// Enable interrupts
@@ -23,25 +26,59 @@ void hw_init() {
 }
 
 void setGreenLed(uint8_t led) {
-	if(led) {
+	if (led) {
 		PORTD |= (1 << PD7);
 	} else {
 		PORTD &= ~(1 << PD7);
 	}
 }
 
+void getTime(struct Time * t) {
+	uint8_t sregTtmp;
+	sregTtmp = SREG; // Sichern
+	cli();
+	// Interrupts deaktivieren
+
+	t->parts = TCNT1;
+	t->time = time;
+
+	SREG = sregTtmp; // Wiederherstellen
+}
+
+/**
+ * c = a - b // c ist minimal 0
+ */
+void timeDiff(struct Time * a, struct Time * b, struct Time * c) {
+	c->parts = 0;
+	c->time = 0;
+
+	if (a->time < b->time) {
+		return;
+	}
+
+	uint8_t timeDiff = 0;
+	if (a->parts < b->parts) {
+		timeDiff = 1;
+	}
+
+	c->parts = a->parts - b->parts;
+	c->time = a->time - b->time;
+}
 
 static uint8_t ledBlink = 0;
 
 /**
  * Timer interrupt
  */
-ISR(TIMER0_COMPA_vect) {
+ISR(TIMER0_COMPA_vect)
+{
 	ledBlink = !ledBlink;
-	if(ledBlink) {
+	if (ledBlink) {
 		PORTD |= (1 << PD6);
 	} else {
 		PORTD &= ~(1 << PD6);
 	}
+
+//TODO: 	time++;
 }
 
--- firmware/Syncboard/driver/hw.h
+++ firmware/Syncboard/driver/hw.h
@@ -11,4 +11,17 @@
 void hw_init();
 void setGreenLed(uint8_t led);
 
+
+struct Time {
+	uint32_t time;
+	uint16_t parts;
+};
+
+void getTime(struct Time * time);
+
+/**
+ * c = a - b
+ */
+void timeDiff(struct Time * a, struct Time * b, struct Time * c);
+
 #endif /* HW_H_ */
