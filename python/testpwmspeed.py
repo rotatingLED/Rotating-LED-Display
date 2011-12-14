47fe08929841ac87d975e8925d838adf842b388d
David Halter <davidhalter88@gmail.com>
2011-12-14 10:59:02 +0100

    server stuff python

--- python/testpwmspeed.py
+++ python/testpwmspeed.py
@@ -11,15 +11,15 @@ for i, pixel in enumerate(img):
     # if i % 192 == j*3+col:
         img[i] = 255
 
-for i in range(10):
+for i in range(100):
     pwm_data = str(bytearray(led.PWM_IMAGE_SIZE))
-    fastpwm.pwm_test(str(img), 0,  pwm_data)
-    #data = fastpwm.pwm_data_4bit_c(str(img), 0, pwm_data)
+    data = fastpwm.pwm_4bit_c(str(img), 0, pwm_data)
 
 for i in range(10):
-    data = fastpwm.pwm_data_4bit(str(img))
+    #data = fastpwm.pwm_data_4bit(str(img))
+    pass
 
-data = led.pwm_data(str(img))
+#data = led.pwm_data(str(img))
 
 #print list(data[:100])
 #print list(pwm_data[:100])
